2b24ea9f72975ed2618a8b7b81e56b80dbcf45b7
Jonathon Kereliuk <kereliuk@Jonathons-MacBook-Pro.local>
2020-04-27 00:33:44 -0400

    made splash nicer

--- lib/midiout/midiout.go
+++ lib/midiout/midiout.go
@@ -11,9 +11,13 @@ import (
 
 // APC20Splash takes a midi stream as input and then flashes the launchpad lights in some way.
 func APC20Splash(out *portmidi.Stream) error {
+	rand.Seed(time.Now().UnixNano())
+	greenOrder, orangeOrder, redOrder := rand.Perm(40), rand.Perm(40), rand.Perm(40)
+	greenSplit, orangeSplit, redSplit := int64(rand.Intn(40)), int64(rand.Intn(40)), int64(rand.Intn(40))
+
 	// Down GREEN
-	for i := int64(0); i < 40; i++ {
-		midiInfo := apc20.IntToMidiMessageDown[i]
+	for i := int64(0); i < greenSplit; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(greenOrder[i])]
 		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, midiInfo.Data2); err != nil {
 			return errors.Wrap(err, "could not write short to midi device")
 		}
@@ -21,8 +25,8 @@ func APC20Splash(out *portmidi.Stream) error {
 	}
 
 	// Down ORANGE
-	for i := int64(39); i > -1; i-- {
-		midiInfo := apc20.IntToMidiMessageDown[i]
+	for i := int64(0); i < orangeSplit; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(orangeOrder[i])]
 		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, 5); err != nil {
 			return errors.Wrap(err, "could not write short to midi device")
 		}
@@ -30,8 +34,31 @@ func APC20Splash(out *portmidi.Stream) error {
 	}
 
 	// Down RED
-	for _, i := range rand.Perm(40) {
-		midiInfo := apc20.IntToMidiMessageDown[int64(i)]
+	for i := int64(0); i < redSplit; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(redOrder[i])]
+		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, 3); err != nil {
+			return errors.Wrap(err, "could not write short to midi device")
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	for i := orangeSplit; i < 40; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(orangeOrder[i])]
+		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, 5); err != nil {
+			return errors.Wrap(err, "could not write short to midi device")
+		}
+		time.Sleep(20 * time.Millisecond)
+	}
+
+	for i := greenSplit; i < 40; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(greenOrder[i])]
+		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, midiInfo.Data2); err != nil {
+			return errors.Wrap(err, "could not write short to midi device")
+		}
+		time.Sleep(20 * time.Millisecond)
+	}
+	for i := redSplit; i < 40; i++ {
+		midiInfo := apc20.IntToMidiMessageDown[int64(redOrder[i])]
 		if err := out.WriteShort(midiInfo.Status, midiInfo.Data1, 3); err != nil {
 			return errors.Wrap(err, "could not write short to midi device")
 		}
